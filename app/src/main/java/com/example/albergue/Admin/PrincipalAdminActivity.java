b1a7647ad57fc5003b487fb0cddd91cc5fe97ad7
eric.benites <eric.benites@pucp.edu.pe>
2021-01-02 00:17:42 -0500

    cerrar sesion

--- app/src/main/java/com/example/albergue/Admin/PrincipalAdminActivity.java
+++ app/src/main/java/com/example/albergue/Admin/PrincipalAdminActivity.java
@@ -2,9 +2,15 @@ package com.example.albergue.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.albergue.MainActivity;
 import com.example.albergue.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class PrincipalAdminActivity extends AppCompatActivity {
 
@@ -12,5 +18,24 @@ public class PrincipalAdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_admin);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_4main, menu);
+        return true;
+    }
+
+    public void logout(MenuItem item){
+        AuthUI instancia = AuthUI.getInstance();
+        instancia.signOut(this).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(PrincipalAdminActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/albergue/Users/MainUserActivity.java
+++ app/src/main/java/com/example/albergue/Users/MainUserActivity.java
@@ -2,9 +2,16 @@ package com.example.albergue.Users;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.albergue.Admin.PrincipalAdminActivity;
+import com.example.albergue.MainActivity;
 import com.example.albergue.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainUserActivity extends AppCompatActivity {
 
@@ -13,4 +20,24 @@ public class MainUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_4main, menu);
+        return true;
+    }
+
+    public void logout(MenuItem item){
+        AuthUI instancia = AuthUI.getInstance();
+        instancia.signOut(this).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(MainUserActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
 }
